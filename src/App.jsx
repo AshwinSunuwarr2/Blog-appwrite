bd1f96ee37d4e9f38a3774905bcada3ed2feb46a
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-04-01 11:05:14 +0545

    rtk setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import {Header, Footer} from "./components";
+import {Outlet} from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>Blog app appwrite
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+  return loading ? ("loading..." ): 
+    <div className="min-h-screen flex flex-wrap content-between bg-slate-200">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+          outlet
+        </main>
+        <Footer/>
+    </div>
+};
+export default App;
